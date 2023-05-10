6284085427dfdd8c92d9b49acf3e66752bb91efd
Tyler Lentz <tlentz@ucsd.edu>
2023-05-10 16:58:43 -0700

    scrolling for line numbers synced with editor window

--- src/lib/TextEditor.svelte
+++ src/lib/TextEditor.svelte
@@ -1,24 +1,33 @@
-<script>
+<script lang="ts">
     import { appWindow } from '@tauri-apps/api/window';
 
     let output = '';
+
     let num_lines = 0;
     let line_nums = '';
 
     let font_size = 12; //pt
 
+    let editor_elem: HTMLTextAreaElement;
+    let line_nums_elem: HTMLTextAreaElement;
+
+    function parseScroll() {
+        line_nums_elem.scrollTop = editor_elem.scrollTop; 
+    }
+
+
     $: num_lines = output.split('\n').length;
     $: line_nums = Array(num_lines).fill(0).map((_, num) => `${num}`).join('\n');
     $: max_line_size = String(num_lines - 1).length * font_size;
 
-    appWindow.listen('open-file', (event) => {
+    appWindow.listen('open-file', (event: any) => {
         output = event.payload.file;
     });
 </script>
 
 <div >
-    <textarea style:font_size class="line_nums" readonly bind:value={line_nums} style:width="{max_line_size}px" ></textarea>
-    <textarea style:font_size class="editor" bind:value={output} ></textarea>
+    <textarea bind:this={line_nums_elem} style:font_size class="line_nums" readonly bind:value={line_nums} style:width="{max_line_size}px" ></textarea>
+    <textarea bind:this={editor_elem} on:scroll={parseScroll} style:font_size class="editor" bind:value={output} ></textarea>
 </div>
 
 <style>
